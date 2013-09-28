479ec46aa03f363be466c069cdbfa304027b0c99
zachary-zzc <shinaider.zhao@gmail.com>
2013-09-29 01:09:16 +0800

    Rearrange file structure. Add Bayesian Linear Regression

---
+++ linear/regression/lr.py
@@ -0,0 +1,93 @@
+import random
+import math
+from numpy import *
+import scipy
+import copy
+import matplotlib.pyplot as plt
+
+
+def leastsqu(dataSet, targetSet, regParam = 0, regularizer = 2):
+	"""
+	**************************************************************************************
+	Use sum of square error function which aroses in the maximum likehood to get liner 
+	regression param set w for supervised learning. To control over-fitting, use regParam 
+	for	regularization
+
+	input: dataSet -- Training dataSet input, eg. [[x11,x12...x1n][x21,x22...x2n]...]
+				 targetSet -- Training dataSet Output, eg. [[y10,y11...y1m][y20,y21...y2m]...],
+											targetSet and dataSet should have same length
+				 regParam, regularizer -- regularization parameter
+
+	output: param set w
+					#voice precisor value b
+	**************************************************************************************
+	"""
+	X = copy.deepcopy(dataSet)
+	for data in X:
+		data.insert(0,1)
+	X = array(X)
+	Y = array(targetSet)
+	
+	try:
+		len(X) == len(Y)
+	except:
+		print 'dataSet and targetSet should have the same length'
+	
+	reg = 0.5*regParam*regularizer*eye(len(X[0]))
+	w = linalg.inv(reg + dot(transpose(X), X))
+	w = dot(w, transpose(X))
+	w = dot(w, Y)
+	
+	#T = Y.sum(axis = 0)/len(Y)
+	#O = X.sum(axis = 0)/len(X)
+	#w[0] = 0
+	#w[0] = T - dot(O, transpose(w)[0])
+
+	drawset(dataSet, targetSet, transpose(postMean)[0].tolist())
+
+	return transpose(w)[0].tolist()
+
+
+
+def bayeslinear(dataSet, targetSet):
+	"""
+	linear Regression use bayesian approach
+	"""
+	X = copy.deepcopy(dataSet)
+	for data in X:
+		data.insert(0,1)
+	X = array(X)
+	Y = array(targetSet)
+	
+	try:
+		len(X) == len(Y)
+	except:
+		print 'dataSet and targetSet should have the same length'
+
+	priorPrecision = 2
+	likehoodSD = 0.2
+	likehoodPrecision = 1/(likehoodSD)**2
+
+	priorMean = zeros((len(X[0]), 1))
+	priorDev = eye(len(X[0]))/priorPrecision
+	
+	postMean = priorMean
+	postDev = priorDev
+
+	for i in xrange(len(dataSet)):
+		data = X[i].reshape(1, len(X[i]))
+		target = Y[i].reshape(1, len(Y[i]))
+
+		postDev = linalg.inv(linalg.inv(priorDev)+likehoodPrecision*dot(transpose(data), data))
+		postMean = dot(postDev, dot(linalg.inv(priorDev), priorMean)+likehoodPrecision*dot(transpose(data), target))
+
+		#if i%2 == 0:
+		#	filename = r'bayes' + str(i)
+		#	drawset(dataSet, targetSet, postMean, filename)
+		priorMean = postMean
+		priorDev = postDev
+	
+	drawset(dataSet, targetSet, transpose(postMean)[0].tolist())
+	return transpose(postMean)[0].tolist(), postDev.tolist() 
+
+
---
+++ plt.py
@@ -0,0 +1,42 @@
+import math
+import mathplotlib.pyplot as plt
+from numpy import *
+
+def drawset(dataSet, targetSet, w = [], filename = ''):
+        """
+        **************************************************************************************
+        Plot Gen Function(green line), Regression Function(red line), Training points(blue points, default zeros)
+        **************************************************************************************
+        """
+        if len(w) == 0:
+                w = [0 for i in range(len(dataSet[0]))]
+
+        randSet = []
+        nSet = []
+        for i in xrange(len(dataSet)):
+                randSet.append(dataSet[i][0])
+                nSet.append(targetSet[i][0])
+
+        x = arange(0, 1, 0.01)
+        basisfunc = []
+        regressionfunc = []
+        for i in x:
+                reg = 0
+                for j in range(len(w)):
+                        reg += w[j]*i**j
+                if(reg**2>=4):
+                        regressionfunc.append(0)
+                else:
+                        regressionfunc.append(reg)
+                #basisfunc.append(math.sin(2*math.pi*i))
+                basisfunc.append(0.5*i-0.3)
+
+        plt.figure()
+        ax = plt.subplot(111)
+        ax.scatter(randSet, nSet, c = 'b')
+        ax.plot(x, basisfunc, c = 'g')
+        ax.plot(x, regressionfunc, c = 'r')
+        if filename != '':
+                plt.savefig(filename + 'png')
+        plt.show()
+
